a89d7c87c0bbaa3b30f431653745f05108aec804
wangpeng237 <2372278438@qq.com>
2019-02-24 09:54:20 +0800

    公共功能部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,4 +9,37 @@ $(function(){
             NProgress.done();
         }, 500)
     })
-})
+});
+
+
+$(function(){
+    $('.clickA').on('click', function(){
+        $(this).next().stop().slideToggle();
+    })
+
+
+    $('.menu').on('click', function(){
+        $('.lt_main').toggleClass('hidemenu');
+        $('.lt_main .title').toggleClass('hidemenu');
+        $('.lt_aside').toggleClass('hidemenu');
+    })
+    
+    // 显示模态框
+    $('.logout').on('click', function(){
+        $('#myModal').modal('show');
+    })
+
+
+    //退出登录
+    $('.modal-footer .btn-primary').on('click', function(){
+        $.ajax({
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function(res){
+                if(res.success){
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+});
